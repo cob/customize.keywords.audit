63939dc6835e3684b7cbad32fdc6f84a3186d55a
Miguel Mesquita <mm@cob.pt>
2022-03-17 19:08:51 +0000

    refactor(customize): keyword.audit new cache and keyword

---
+++ templates/keywords/audit/frontend/js/cob/_audit.js
@@ -0,0 +1,17 @@
+//----------------- $audit  ------------------------
+cob.custom.customize.push(function (core, utils, ui) {
+    core.customizeAllInstances((instance, presenter) => 
+    {
+        let userFPs = presenter.findFieldPs( fp => /[$]audit\.(creator|updater)\.(username|usermRef)/.exec(fp.field.fieldDefinition.description && fp.field.fieldDefinition.description) )
+        userFPs.forEach( fp => {
+            fp.disable()
+            if(!instance.isNew() || presenter.isGroupEdit()) return //Only update if it's on create interface (updates will only be changed by the backend)
+            if(/[$]audit\.(creator|updater)\.username/.exec(fp.field.fieldDefinition.description)) {
+                fp.setValue(core.getCurrentLoggedInUser())
+            }
+            if(/[$]audit\.(creator|updater)\.usermRef/.exec(fp.field.fieldDefinition.description)) {
+                fp.setValue(core.getCurrentLoggedInUserUri())
+            }
+        })
+    })
+});
